8aca3a59eed385834af7bd18742c047a52141689
olegok13 <olegok14@gmail.com>
2021-06-15 19:12:57 +0300

    add work

---
+++ scroll2.js
@@ -0,0 +1,9 @@
+// $(window).scroll(function () {
+//   var windowWidth = $(this).width();
+//   var windowHeight = $(this).height();
+//   var windowScrollTop = $(this).scrollTop();
+
+//   if ($(window).width() > 0) {
+//     thirdAnimation();
+//   }
+// });
